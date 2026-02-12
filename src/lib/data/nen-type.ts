383a6e5c85fd9a1511cdb253e75096e8f46f06ab
kevinkeyssx <keyss1700@gmail.com>
2026-02-12 19:40:40 -0300

    NEN_TYPE_MAP added

---
+++ src/lib/data/nen-type.ts
@@ -0,0 +1,8 @@
+export const NEN_TYPE_MAP: Record<string, string> = {
+    'INT' : 'Intensificación',
+    'TRA' : 'Transmutación',
+    'MAT' : 'Materialización',
+    'ESP' : 'Especialización',
+    'MAN' : 'Manipulación',
+    'EMI' : 'Emisión'
+};
